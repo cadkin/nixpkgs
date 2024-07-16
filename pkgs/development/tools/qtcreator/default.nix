3c78aee58953340ba4f963c92d27b03538ff05e9
cadkin <cva@siliconslumber.net>
2024-07-16 13:49:24 -0400

    qtcreator: bump to 14.0.0-rc1

--- pkgs/development/tools/qtcreator/default.nix
+++ pkgs/development/tools/qtcreator/default.nix
@@ -30,11 +30,11 @@
 
 stdenv.mkDerivation rec {
   pname = "qtcreator";
-  version = "13.0.2";
+  version = "14.0.0-rc1";
 
   src = fetchurl {
-    url = "https://download.qt.io/official_releases/${pname}/${lib.versions.majorMinor version}/${version}/qt-creator-opensource-src-${version}.tar.xz";
-    hash = "sha256-wSXMVSJhnH+PwoBadQq5bLu1al/fw4i2yxWrda9+wM4=";
+    url = "https://download.qt.io/development_releases/${pname}/${lib.versions.majorMinor version}/${version}/qt-creator-opensource-src-${version}.tar.xz";
+    hash = "sha256-sth3z2eLef9houqAKqcS5YhKV1LOAEaUNX4og+Y3lsU=";
   };
 
   nativeBuildInputs = [
